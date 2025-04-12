8ac4d668f2939774d9ac2d782ff7369fb28fe5ab
Peter John Story <pete@norwegianstory.net>
2025-04-12 20:08:25 +0200

    sort latest result pdfs by name not modification time, so it works properly on github machines I guess it has the advantage of being more transparent as well when you look at the file system.

--- testutils.py
+++ testutils.py
@@ -7,5 +7,9 @@ from pathlib import Path
 def getLatestResultFile(albumFolderBasename, pattern: str) -> str:
     resultpdfpattern = str(Path(Path.cwd(), 'tests', f"{albumFolderBasename}", 'previous_result_pdfs', pattern))
     resultpdffiles = glob.glob(resultpdfpattern)
-    resultpdffiles.sort(key=os.path.getmtime, reverse=True)
+    # I used to sort on the modification date in order to find the latest file:
+    #   resultpdffiles.sort(key=os.path.getmtime, reverse=True)
+    # but that doesn't work when we run automated tests on github machines. So
+    # I guess we'll just have to rely on the naming convention:
+    resultpdffiles.sort(key=os.path.basename, reverse=True)
     return resultpdffiles[0] if len(resultpdffiles) > 0 else None
