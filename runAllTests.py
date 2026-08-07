fd5a2d26524e2acadfca6f568739442899978bdc
Pete Story <pete@norwegianstory.net>
2026-08-07 22:53:08 +0200

    Improves the font shrinking, frame increasing, code. Also makes the diagnostic better, so the user knows what to do

--- runAllTests.py
+++ runAllTests.py
@@ -1,4 +1,5 @@
 import os
 import pytest
 os.environ['IGNORELOCALFONTS'] = "1"
-pytest.main(['-x', '--capture=tee-sys', '.', ''])
+pytest.main(['-x', '--capture=tee-sys', '.', '']) # to stop after the first assertion
+# pytest.main(['--capture=tee-sys', '--maxfail=0', '.']) # to continue after an assertion
